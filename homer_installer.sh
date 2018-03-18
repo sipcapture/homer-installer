13e07a3650ab9416b9c2b0e7c7183386726fd080
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-18 15:00:07 +0100

    Testing heplify-server setup

--- homer_installer.sh
+++ homer_installer.sh
@@ -10,6 +10,7 @@
 #
 #  Copyright notice:
 #
+#  (c) 2018 QXIP BV
 #  (c) 2011-2016 Lorenzo Mangani <lorenzo.mangani@gmail.com>
 #  (c) 2011-2016 Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
 #
@@ -69,7 +70,7 @@ OS=""
 DISTRO=""
 DISTRO_VERSION=""
 WEB_ROOT=""
-KAMAILIO_VERSION="5"
+HEPLIFY_VERSION="1"
 
 ######################################################################
 #
@@ -301,10 +302,10 @@ create_or_update_misc() {
 }
 
 create_or_update_config() {
-  # This function copies the deafult configuration files for homer and kamailio into place
+  # This function copies the deafult configuration files for homer and heplify into place
 
-  local kamailio_version=${1:-"5"}
-  KAMAILIO_VERSION="$kamailio_version"
+  local heplify_version=${1:-"1"}
+  HEPLIFY_VERSION="$heplify_version"
   local overwrite_dst=${2:-"yes"}
   local -a cfg_files=(
                        "$src_base_dir/$src_homer_config_dir/docker/configuration.php|$web_doc_root/api/configuration.php" \
@@ -315,9 +316,8 @@ create_or_update_config() {
   cmd_cp=$(locate_cmd "cp")
   cmd_chmod=$(locate_cmd "chmod")
 
-  case "$KAMAILIO_VERSION" in
-    4 ) cfg_files+=("${cfg_files[@]}" "/usr/src/homer-config/sipcapture/sipcapture.kamailio|/etc/kamailio/kamailio.cfg") ;;
-    5 ) cfg_files+=("${cfg_files[@]}" "/usr/src/homer-config/sipcapture/sipcapture.kamailio5|/etc/kamailio/kamailio.cfg") ;;
+  case "$HEPLIFY_VERSION" in
+    1 ) cfg_files+=("${cfg_files[@]}" "/usr/src/homer-config/sipcapture/sipcapture.heplify1|$heplify_root/heplify-server.toml") ;;
   esac
 
   local original_ifs=$IFS
@@ -581,13 +581,13 @@ config_search_and_replace() {
     -e "s/{{ DB_HOST }}/$DB_HOST/g" \
     "$web_doc_root/api/configuration.php"
 
-  # Kamailio Scripts
+  # Heplify configuration
   $cmd_sed -i \
-    -e "s/homer_user/$DB_USER/g" \
-    -e "s/homer_password/$DB_PASS/g" \
-    -e "s/127\.0\.0\.1/$DB_HOST/g" \
+    -e "s/HOMER_DB_USER/$DB_USER/g" \
+    -e "s/HOMER_DB_PASSWORD/$DB_PASS/g" \
+    -e "s/HOMER_DB/$DB_HOST/g" \
     -e "s/9060/$LISTEN_PORT/g" \
-    /etc/kamailio/kamailio.cfg
+    $heplify_root/heplify-server.toml
 
   # Apache docroot
   $cmd_sed -i \
@@ -595,10 +595,10 @@ config_search_and_replace() {
     $web_cfg_root/sipcapture.conf
 }
 
-create_kamailio_service() {
+create_heplify_service() {
   local sys_systemd_base='/lib/systemd/system'
   local usr_systemd_base='/etc/systemd/system'
-  local sys_kamailio_svc='kamailio.service'
+  local sys_heplify_svc='heplify-server.service'
   local sys_mysql_svc=''
 
   local cmd_systemctl=$(locate_cmd "systemctl")
@@ -612,39 +612,32 @@ create_kamailio_service() {
       sys_mysql_svc=mysqld.service
     fi
 
-    if [ ! -f $sys_systemd_base/$sys_kamailio_svc ]; then
-      $cmd_cat << __EOFL__ > $sys_systemd_base/$sys_kamailio_svc
+    if [ ! -f $sys_systemd_base/$sys_heplify_svc ]; then
+      $cmd_cat << __EOFL__ > $sys_systemd_base/$sys_heplify_svc
 [Unit]
-Description=Kamailio (OpenSER) - the Open Source SIP Server
+Description=SIPCAPTURE HEP Server
 After=network.target
 
 [Service]
-Type=forking
-Environment='CFGFILE=/etc/kamailio/kamailio.cfg'
-Environment='SHM_MEMORY=64'
-Environment='PKG_MEMORY=8'
-Environment='USER=kamailio'
-Environment='GROUP=kamailio'
-EnvironmentFile=-/etc/default/kamailio
-EnvironmentFile=-/etc/default/kamailio.d/*
-# PIDFile requires a full absolute path
-PIDFile=/var/run/kamailio/kamailio.pid
-# ExecStart requires a full absolute path
-ExecStart=/usr/sbin/kamailio -P /var/run/kamailio/kamailio.pid -f \$CFGFILE -m \$SHM_MEMORY -M \$PKG_MEMORY -u \$USER -g \$GROUP
-Restart=on-abort
+WorkingDirectory=/opt/heplify-server
+ExecStart=/opt/heplify-server/heplify-server
+ExecStop=/bin/kill ${MAINPID}
+Restart=on-failure
+RestartSec=10s
+Type=simple
 
 [Install]
 WantedBy=multi-user.target
 __EOFL__
       check_status "$?"
     fi  
-    if [ ! -d $usr_systemd_base/${sys_kamailio_svc}.d ]; then
-      $cmd_mkdir -m 0755 -p $usr_systemd_base/${sys_kamailio_svc}.d
+    if [ ! -d $usr_systemd_base/${sys_heplify_svc}.d ]; then
+      $cmd_mkdir -m 0755 -p $usr_systemd_base/${sys_heplify_svc}.d
       check_status "$?"
     fi
-    if [ ! -f $usr_systemd_base/${sys_kamailio_svc}.d/require_mysql.conf ] && \
+    if [ ! -f $usr_systemd_base/${sys_heplify_svc}.d/require_mysql.conf ] && \
        [ ! -z "$sys_mysql_svc" ]; then
-      $cmd_cat << __EOFL__ > $usr_systemd_base/${sys_kamailio_svc}.d/require_mysql.conf
+      $cmd_cat << __EOFL__ > $usr_systemd_base/${sys_heplify_svc}.d/require_mysql.conf
 [Unit]
 After= $sys_mysql_svc
 __EOFL__
@@ -707,11 +700,11 @@ banner_end() {
   echo "         '$WEB_ROOT/api/configuration.php'"
   echo "         '$WEB_ROOT/api/preferences.php'"
   echo
-  echo "     * Verify capture settings for Homer/Kamailio:"
-  echo "         '/etc/kamailio/kamailio.cfg'"
+  echo "     * Verify capture settings for Homer/Heplify:"
+  echo "         '/opt/heplify/heplify-server.toml'"
   echo
   echo "     * Start/stop Homer SIP Capture:"
-  echo "         '/sbin/kamctl start|stop'"
+  echo "         'systemctl heplify start|stop'"
   echo
   echo "     * Access HOMER UI:"
   echo "         http://$my_primary_ip"
@@ -766,15 +759,14 @@ setup_centos_7() {
                        gnutls-devel openssl openssl-devel mod_ssl perl patch rsyslog \
                        unzip zip zlib zlib-devel bison flex pcre-devel libxml2-devel \
                        sox httpd php php-gd php-mysql php-json git php-mysql php-devel"
-  local kamailio_pkg_list="kamailio kamailio-outbound kamailio-sctp kamailio-tls \
-                           kamailio-websocket kamailio-jansson kamailio-mysql"
   local mysql_pkg_list="libdbi-dbd-mysql perl-DBD-MySQL mysql-community-server mysql-community-client"
-  local -a service_names=("mysqld" "kamailio" "httpd")
+  local -a service_names=("mysqld" "heplify-server" "httpd")
   local web_cfg_root="/etc/httpd/conf.d"
   local web_doc_root="/var/www/html/homer"
   WEB_ROOT=$web_doc_root # WEB_ROOT used in banner_end function
   local web_ownership="apache:apache"
   local mnt_script_dir="/opt/homer"
+  local heplify_root="/opt/heplify"
   local src_base_dir="/usr/src"
   local src_homer_ui_dir="homer-ui"
   local src_homer_api_dir="homer-api"
@@ -788,11 +780,9 @@ setup_centos_7() {
   $cmd_yum -q -y install "https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm"
   # check_status "$?"
 
-  case "$KAMAILIO_VERSION" in
-    4 ) $cmd_wget --inet4-only --quiet --output-document=/etc/yum.repos.d/kamailio:v4.4.x-rpms.repo \
-    "http://download.opensuse.org/repositories/home:/kamailio:/v4.4.x-rpms/CentOS_7/home:kamailio:v4.4.x-rpms.repo" ;;
-    5 ) $cmd_wget --inet4-only --quiet --output-document=/etc/yum.repos.d/kamailio:v5.0.x-rpms.repo \
-    "http://download.opensuse.org/repositories/home:/kamailio:/v5.0.x-rpms/CentOS_7/home:kamailio:v5.0.x-rpms.repo" ;;
+  case "$HEPLIFY_VERSION" in
+    1 ) $cmd_wget --inet4-only --quiet --output-document=$heplify_root/heplify-server \
+    "https://github.com/sipcapture/heplify-server/releases/download/v1/heplify-server" ;;
   esac
   check_status "$?"
 
@@ -801,10 +791,10 @@ setup_centos_7() {
   $cmd_yum -y update
   check_status "$?"
 
-  $cmd_yum -y install $base_pkg_list $kamailio_pkg_list $mysql_pkg_list
+  $cmd_yum -y install $base_pkg_list $mysql_pkg_list
   # check_status "$?"
 
-  create_kamailio_service
+  create_heplify_service
 
   for svc in ${service_names[@]}; do
     $cmd_chkconfig "$svc" on
@@ -848,14 +838,9 @@ setup_debian_8() {
   local base_pkg_list="ca-certificates curl apache2 libapache2-mod-php5 php5 \
                        php5-cli php5-gd php-pear php5-dev php5-mysql php5-json \
                        php-services-json git wget pwgen rsyslog perl libdbi-perl libclass-dbi-mysql-perl"
-  local kamailio_pkg_list="kamailio rsyslog kamailio-outbound-modules kamailio-geoip-modules \
-                           kamailio-sctp-modules kamailio-tls-modules kamailio-websocket-modules \
-                           kamailio-utils-modules kamailio-mysql-modules kamailio-extra-modules \
-                           geoip-database geoip-database-extra"
   local mysql_pkg_list="mysql-server libmysqlclient18"
-  local -a service_names=("mysql" "kamailio" "apache2")
+  local -a service_names=("mysql" "heplify-server" "apache2")
   local -a repo_keys=(
-                       'kamailio50|FB40D3E6508EA4C8' \
                        'mysql57|8C718D3B5072E1F5'
                      )
   local web_cfg_root="/etc/apache2/sites-available"
@@ -863,6 +848,7 @@ setup_debian_8() {
   WEB_ROOT=$web_doc_root # WEB_ROOT used in banner_end function
   local web_ownership="www-data:www-data"
   local mnt_script_dir="/opt/homer"
+  local heplify_root="/opt/heplify"
   local src_base_dir="/usr/src"
   local src_homer_ui_dir="homer-ui"
   local src_homer_api_dir="homer-api"
@@ -875,13 +861,9 @@ setup_debian_8() {
   local cmd_ln=$(locate_cmd "ln")
   local cmd_update_rcd=$(locate_cmd "update-rc.d")
 
-  case "$KAMAILIO_VERSION" in
-    4 ) echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.7" > /etc/apt/sources.list.d/mysql.list && \
-            echo "deb http://deb.kamailio.org/kamailio44 jessie main" > /etc/apt/sources.list.d/kamailio44.list && \
-            echo "deb-src http://deb.kamailio.org/kamailio44 jessie main" >> /etc/apt/sources.list.d/kamailio44.list ;;
-    5 ) echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.7" > /etc/apt/sources.list.d/mysql.list && \
-            echo "deb http://deb.kamailio.org/kamailio50 jessie main" > /etc/apt/sources.list.d/kamailio50.list && \
-            echo "deb-src http://deb.kamailio.org/kamailio50 jessie main" >> /etc/apt/sources.list.d/kamailio50.list ;;
+  case "$HEPLIFY_VERSION" in
+    1 ) $cmd_wget --inet4-only --quiet --output-document=$heplify_root/heplify-server \
+    "https://github.com/sipcapture/heplify-server/releases/download/v1/heplify-server" ;;
   esac
  
   local original_ifs=$IFS
@@ -894,9 +876,9 @@ setup_debian_8() {
 
   $cmd_apt_get update -qq
   DEBIAN_FRONTEND=noninteractive $cmd_apt_get install --no-install-recommends --no-install-suggests -yqq \
-    $base_pkg_list $kamailio_pkg_list $mysql_pkg_list
+    $base_pkg_list $mysql_pkg_list
 
-  create_kamailio_service
+  create_heplify_service
   repo_clone_or_update "$src_base_dir" "$src_homer_api_dir" "https://github.com/sipcapture/homer-api.git"
   repo_clone_or_update "$src_base_dir" "$src_homer_ui_dir" "https://github.com/sipcapture/homer-ui.git"
   repo_clone_or_update "$src_base_dir" "$src_homer_config_dir" "https://github.com/sipcapture/homer-config.git"
@@ -927,20 +909,6 @@ setup_debian_8() {
 
   config_search_and_replace
   $mnt_script_dir/homer_rotate
-
-  if [[ -d /usr/lib/x86_64-linux-gnu/kamailio ]]; then
-    if [[ ! -e /usr/lib64 ]]; then
-      $cmd_ln -r -f -s /usr/lib /usr/lib64
-    fi
-    if [[ ! -e /usr/lib64/kamailio ]]; then
-      $cmd_ln -r -f -s /usr/lib/x86_64-linux-gnu/kamailio /usr/lib64/kamailio
-    fi
-  else
-    if [[ ! -d /usr/lib/x86_64-linux-gnu ]]; then
-      mkdir -p /usr/lib/x86_64-linux-gnu
-      $cmd_ln -r -f -s /usr/lib64/kamailio /usr/lib/x86_64-linux-gnu/kamailio
-    fi
-  fi
 
   local cmd_a2enmod=$(locate_cmd "a2enmod")
   $cmd_a2enmod rewrite
